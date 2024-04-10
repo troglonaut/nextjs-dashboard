0977532a11caffc27c636b4e95cac56451326bd8
Eric <ericscottrichards@gmail.com>
2024-04-10 17:51:26 -0600

    add search and pagination

--- app/(ui)/dashboard/invoices/page.tsx
+++ app/(ui)/dashboard/invoices/page.tsx
@@ -1,3 +1,40 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import { Suspense } from 'react';
+import Search from '../../search';
+import { CreateInvoice } from './buttons';
+import { InvoicesTableSkeleton } from '../../skeletons';
+import Table from './table';
+import Pagination from './pagination';
+import { fetchInvoicesPages } from '@/app/lib/data';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    page?: string;
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`lusitana.className text-2xl`}>Invoices</h1>
+      </div>
+      <div className="gap-w mt-4 flex items-center justify-between md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+
+      <div className="mt-4 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
