513c0cdbc46fc47908808d0f02d80b67e035a388
Eric <ericscottrichards@gmail.com>
2024-04-11 16:14:34 -0600

    add defaultChecked and defaultValue for edit invoice

--- app/(ui)/dashboard/invoices/create-form.tsx
+++ app/(ui)/dashboard/invoices/create-form.tsx
@@ -63,6 +63,7 @@ export default function Form({
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                defaultValue={invoice?.amount}
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -83,6 +84,7 @@ export default function Form({
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  defaultChecked={invoice?.status === 'pending'}
                 />
                 <label
                   htmlFor="pending"
@@ -98,6 +100,7 @@ export default function Form({
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  defaultChecked={invoice?.status === 'paid'}
                 />
                 <label
                   htmlFor="paid"
